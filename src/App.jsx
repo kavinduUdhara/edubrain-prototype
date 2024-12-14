f23b7aaf7d6aa221d562419e51d0771135a34568
kavinduUdhara <wmkavinduudhara@gmail.com>
2024-12-14 21:51:44 +0530

    feat: refactor routing to use AuthVerifiedLayout for protected routes and update sign-in path

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
+import AuthProtectedLayout from "./layout/AuthProtecetdLayout";
+import AuthVerifiedLayout from "./layout/AuthVerfiedLayout";
 import "./App.css";
 import Home from "./pages/home/Home";
 import Dashboard from "./pages/dashboard/Dashboard";
 import PaperPractice from "./pages/paperPractice/paperPractice";
-import AuthProtectedLayout from "./layout/AuthProtecetdLayout";
 import Login from "./pages/signIn/SignIn";
 import AttemptSummaryPage from "./pages/paperPractice/AttemptSummary/AttemptSummary";
+import RegisterPage from "./pages/signIn/register/Register";
 
 import { auth } from "./firebase";
 import { useEffect } from "react";
@@ -19,15 +21,15 @@ function App() {
   // Google Analytics user ID tracking
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
-      window.gtag('js', new Date());
+      window.gtag("js", new Date());
       if (user) {
         // Send user ID to Google Analytics
-        window.gtag("config", 'G-V1V8X1QF8Q', {
+        window.gtag("config", "G-V1V8X1QF8Q", {
           user_id: user.uid, // Send the logged-in user's ID
         });
       } else {
         // Clear user ID when not logged in
-        window.gtag("config", 'G-V1V8X1QF8Q', {
+        window.gtag("config", "G-V1V8X1QF8Q", {
           user_id: null, // Clear user ID if logged out
         });
       }
@@ -36,7 +38,7 @@ function App() {
     // Clean up the listener when component unmounts
     return () => unsubscribe();
   }, []);
-  
+
   return (
     <Router>
       {/* Dynamic Navigation */}
@@ -44,30 +46,38 @@ function App() {
       {/* Define Routes */}
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/log-in" element={<Login />} />
+        <Route path="/sign-in" element={<Login />} />
+        <Route
+          path="/log-in"
+          element={
+            <AuthProtectedLayout>
+              <RegisterPage />
+            </AuthProtectedLayout>
+          }
+        />
         <Route
           path="/dashboard"
           element={
-            <AuthProtectedLayout>
+            <AuthVerifiedLayout>
               <Dashboard />
-            </AuthProtectedLayout>
+            </AuthVerifiedLayout>
           }
         />
         <Route
           path="/paper-practice/:pp_id"
           element={
-            <AuthProtectedLayout>
+            <AuthVerifiedLayout>
               <PaperPractice />
-            </AuthProtectedLayout>
+            </AuthVerifiedLayout>
           }
         />
         <Route path="/paper-practice" element={<Navigate to="/dashboard" />} />
         <Route
           path="/paper-attempt/:att_id"
           element={
-            <AuthProtectedLayout>
+            <AuthVerifiedLayout>
               <AttemptSummaryPage />
-            </AuthProtectedLayout>
+            </AuthVerifiedLayout>
           }
         />
       </Routes>
