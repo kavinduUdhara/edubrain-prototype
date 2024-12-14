1d4b4e3106f0d57611ea3b281f21551b7b2272b9
kavinduUdhara <wmkavinduudhara@gmail.com>
2024-12-15 03:12:21 +0530

    feat: refactor Dashboard layout by integrating PageHolder component and cleaning up CSS styles

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -16,63 +16,16 @@ import { UnitAvgMarksChart } from "./BarChart";
 import UnitsFlowChart from "@/components/UnitsOverviewTree";
 import { useNavigate } from "react-router-dom";
 import { PaperListPopUp } from "./PaperListPopUp";
+import PageHolder from "@/layout/PageHolder";
 
 export default function Dashboard() {
   const user = auth.currentUser;
   const navigate = useNavigate();
 
   return (
-    <div className="page-holder">
-      <div className="dashboard-page">
-        <div className="def-holder top-holder">
-          <div className="def-child top-menu">
-            <div className="left">
-              <button
-                onClick={() => {
-                  navigate("/");
-                }}
-                className="logo-holder"
-              >
-                <div className="logo-icon-holder">
-                  <img src="/main-logo.svg" />
-                </div>
-                <h1>
-                  EduBrain{" "}
-                  <span>
-                    <ImLab />
-                  </span>
-                </h1>
-              </button>
-            </div>
-            <div className="right">
-              <button className="profile-info-holder">
-                <div className="profile-holder">
-                  <img src={user.photoURL} />
-                </div>
-                <div className="welcome-txt">
-                  <h1>Kavindu Udhara</h1>
-                  <p>@kavinduudhara</p>
-                  {/* <p>
-                  Welcome to EduBrain{" "}
-                  <span>
-                    <ImLab /> Prototype
-                  </span>
-                </p> */}
-                </div>
-              </button>
-            </div>
-
-            {/* <img
-              src={flotingObjectsImg}
-              alt="floating objects"
-              className="flo-obj-img"
-            /> */}
-          </div>
-        </div>
-        <div className="bottom-holder">
-          <div className=""></div>
-          <div className="def-child content gap-5">
-            {/* <div className="ai-s-child">
+    <PageHolder maxW={5} >
+      <div className="def-child content gap-5 p-3 px-5">
+        {/* <div className="ai-s-child">
               <h1 className="title">
                 {" "}
                 <RiGeminiFill />
@@ -85,39 +38,39 @@ export default function Dashboard() {
               </div>
               <div className="sug-list"></div>
             </div> */}
-            <div className="overview-sec fetures-sec">
-              <h1 className="title">
-                <FaUncharted /> Numbers to wrap your head around
+        <div className="overview-sec fetures-sec">
+          <h1 className="title">
+            <FaUncharted /> Numbers to wrap your head around
+          </h1>
+          <div className="flex flex-wrap gap-3">
+            <UnitWeightChart />
+            <UnitAvgMarksChart />
+          </div>
+        </div>
+        <div className="fetures-sec">
+          <h1 className="title">
+            <GiPartyPopper /> Fetrues on{" "}
+            <span>
+              <ImLab /> prototype
+            </span>
+          </h1>
+          <div className="f-list">
+            <div className="f-item">
+              <h1>
+                <LucideNewspaper />
+                Past Paper Practice
               </h1>
-              <div className="flex flex-wrap gap-3">
-                <UnitWeightChart />
-                <UnitAvgMarksChart />
-              </div>
+              <p>
+                Students can practice exam questions and receive AI-powered
+                insights on areas needing improvement.
+              </p>
+              <PaperListPopUp>
+                <button>
+                  Practice Past Papers <RiExternalLinkFill />
+                </button>
+              </PaperListPopUp>
             </div>
-            <div className="fetures-sec">
-              <h1 className="title">
-                <GiPartyPopper /> Fetrues on{" "}
-                <span>
-                  <ImLab /> prototype
-                </span>
-              </h1>
-              <div className="f-list">
-                <div className="f-item">
-                  <h1>
-                    <LucideNewspaper />
-                    Past Paper Practice
-                  </h1>
-                  <p>
-                    Students can practice exam questions and receive AI-powered
-                    insights on areas needing improvement.
-                  </p>
-                  <PaperListPopUp>
-                    <button>
-                      Practice Past Papers <RiExternalLinkFill />
-                    </button>
-                  </PaperListPopUp>
-                </div>
-                {/* <div className="f-item">
+            {/* <div className="f-item">
                   <h1>
                     <MdOutlineChatBubbleOutline /> AI-Enhanced Forum
                   </h1>
@@ -130,11 +83,9 @@ export default function Dashboard() {
                     Ask a questions <RiExternalLinkFill />
                   </button>
                 </div> */}
-              </div>
-            </div>
           </div>
         </div>
       </div>
-    </div>
+    </PageHolder>
   );
 }
